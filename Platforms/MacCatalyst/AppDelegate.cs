27dd743510e8108b951852453f2aed1fe75f1ac1
gdemartino93 <g.demartino9393@gmail.com>
2025-03-06 23:04:51 +0100

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MAUI_UnitConverter;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
